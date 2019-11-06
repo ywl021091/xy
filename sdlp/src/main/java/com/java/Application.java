fc7035b9b2c1dae6fbfd5460c945c40743352e6a
ywl <ywl@DESKTOP-J4G020J>
2019-11-06 09:41:47 +0800

    地图等级的更改

--- sdlp/src/main/java/com/java/Application.java
+++ sdlp/src/main/java/com/java/Application.java
@@ -5,22 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@MapperScan({"com.java"})
-//包扫描：扫描controller:service
+//鍖呮壂鎻忥細鎵弿controller:service
 //@SpringBootApplication(scanBasePackages = {"com.java"})
-//开启sprinngboot的默认配置
+//寮�鍚痵prinngboot鐨勯粯璁ら厤缃�
 //@EnableAutoConfiguration
 //@ComponentScan(basePackages = {"com.java"})
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
-	/*外置tomcat使用*/
+	/*澶栫疆tomcat浣跨敤*/
 	/*@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
 		return application.sources(Application.class);
 	}*/
 
     
     public static void main(String[] args) {
-        // 内置了tomcat，因此springboot可以不用发布在外部的tomcat服务器中
-        // spring就是简化了ssm框架的搭建,类似于不用书写ssm的所有配置文件
+        // 鍐呯疆浜唗omcat锛屽洜姝pringboot鍙互涓嶇敤鍙戝竷鍦ㄥ閮ㄧ殑tomcat鏈嶅姟鍣ㄤ腑
+        // spring灏辨槸绠�鍖栦簡ssm妗嗘灦鐨勬惌寤�,绫讳技浜庝笉鐢ㄤ功鍐檚sm鐨勬墍鏈夐厤缃枃浠�
         SpringApplication.run(Application.class, args);
     }
     
